dad45a127a140ebf8e053b4f9944e752b011a124
MI_C_12\TamagnaD <supernikx@gmail.com>
2018-04-10 12:45:45 +0200

    Update all'organizzazione delle cartelle/scena

--- Assets/Scripts/Enemies/EnemyBonus.cs
+++ Assets/Scripts/Enemies/EnemyBonus.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBonus : EnemyBase {
-    [Header("Drop Settings")]
     protected override ObjectTypes getID()
     {
         return ObjectTypes.EnemyBonus;
--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour {
 
     PoolManager pool;
-    public float spawnTime;
-    float spawnTimer, screenHeight, screenWidth;
+    public float spawnTime,BonusSpawnTime;
+    float spawnTimer, bonusSpawnTimer, screenHeight, screenWidth;
     public List<EnemyBase> enemyTypes = new List<EnemyBase>();
+    public List<EnemyBase> enemyBonus = new List<EnemyBase>();
 
 	void Start () {
         pool = PoolManager.instance;
@@ -17,12 +18,21 @@ public class SpawnManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         spawnTimer += Time.deltaTime;
+        bonusSpawnTimer += Time.deltaTime;
         if (spawnTimer > spawnTime)
         {
             EnemyBase enemyPooled = pool.GetPooledObject(enemyTypes[Random.Range(0, enemyTypes.Count)].objectID,gameObject).GetComponent<EnemyBase>();
             screenWidth = Camera.main.orthographicSize * Camera.main.aspect - enemyPooled.transform.localScale.magnitude;
             enemyPooled.Spawn(new Vector3(Random.Range(screenWidth, -screenWidth), transform.position.y, screenHeight));
             spawnTimer = 0f;
+        }
+
+        if (bonusSpawnTimer > BonusSpawnTime)
+        {
+            EnemyBase enemyPooled = pool.GetPooledObject(enemyBonus[Random.Range(0, enemyBonus.Count)].objectID, gameObject).GetComponent<EnemyBase>();
+            screenWidth = Camera.main.orthographicSize * Camera.main.aspect - enemyPooled.transform.localScale.magnitude;
+            enemyPooled.Spawn(new Vector3(Random.Range(screenWidth, -screenWidth), transform.position.y, screenHeight));
+            bonusSpawnTimer = 0f;
         }
 	}
 }
